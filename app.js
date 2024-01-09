c43236be1c3bc62e3312b99cf947edd6e995c71b
Tanawut Wongboot <tanawut.w@outlook.com>
2024-01-09 13:27:36 +0700

    feat: add new middlewares called logging

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import assignmentRouter from "./apps/assignments.js";
+import logging from "./middlewares/logging.js";
 
 const app = express();
 const port = 4000;
+
+//use logging Middleware before other Routes
+app.use(logging);
 
 app.use(bodyParser.json());
 app.use("/assignments", assignmentRouter);
