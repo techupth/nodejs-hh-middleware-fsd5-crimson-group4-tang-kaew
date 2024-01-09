085ef6d8558a7068a6eba853ba86a2396c443fe8
Tanawut Wongboot <tanawut.w@outlook.com>
2024-01-09 14:30:28 +0700

    feat: add validation for passing json data

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ import express from "express";
 import bodyParser from "body-parser";
 import assignmentRouter from "./apps/assignments.js";
 import logging from "./middlewares/logging.js";
+import validateAssignmentData from "./middlewares/validateAssignmentData.js";
 
 const app = express();
 const port = 4000;
 
-//use logging Middleware before other Routes
-app.use(logging);
+// Chaining Middleware
+// use logging Middleware before other Routes
+app.use(bodyParser.json()).use(logging).use(validateAssignmentData);
 
-app.use(bodyParser.json());
 app.use("/assignments", assignmentRouter);
 
 app.listen(port, () => {
